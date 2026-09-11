42b864feb1b33a04206021344915981998eb3600
AlisaPya <sato.alisa.pya@gmail.com>
2026-09-11 15:49:28 +0300

    added VContainer template

---
+++ Assets/Game/Scripts/Bootstrap/MenuLifetimeScope.cs
@@ -0,0 +1,12 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace Game.Scripts.Bootstrap
+{
+    public class MenuLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Bootstrap/RootLifetimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Game.Scripts.Bootstrap
+{
+    public class RootLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
